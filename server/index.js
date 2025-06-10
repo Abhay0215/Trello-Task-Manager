174a58b1de7ae52d143f55914c07676386ff2211
Abhay Rana <abhayrana089@gmail.com>
2025-06-10 23:42:40 +0530

    ✅ Added backend for register and sign in but some error lingers will see later

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+const authRoutes = require("./routes/auth");
+const cors = require("cors")
+
+const authMiddleWare = require("./middleware/middleWare")
+const User = require('./models/User.js');
+
+dotenv.config();
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/protected", authMiddleWare, (req, res) => {
+    res.json({message: " acees granted", user: req.user});
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => app.listen(5000, () => console.log("server is runnnnyy.y.y.y."))) .catch ((err) => console.log(err));
